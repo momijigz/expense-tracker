c4f9d613a9d671f8ebdcafcdb885df2216b5a360
Moazzam Khan <moazzam.khan.wa@gmail.com>
2020-02-22 22:53:12 +0500

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import HeaderComponent from './components/header.component';
+import './App.css';
+import BalanceComponent from './components/balance.component';
+import IncomeExpenseComponent from './components/income-expense.component';
+import TransactionListComponent from './components/transaction-list.component';
 
 function App() {
   return (
     <div className="App">
+      <HeaderComponent />
+      <div className="container">
+        <BalanceComponent />
+        <IncomeExpenseComponent />
+        <TransactionListComponent />
+      </div>
     </div>
   );
 }
---
+++ src/components/add-transaction.component.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const AddTransactionComponent = () => {
+	const [text, setText] = useState("");
+	const [amount, setAmount] = useState(0);
+
+	return (
+		<div>
+			<h3>Add new transaction</h3>
+			<form>
+				<div className="form-control">
+					<label htmlfor="text">Text</label>
+					<input type="text" placeholder="Enter text..." />
+				</div>
+				<div className="form-control">
+					<label htmlfor="amount">
+						Amount <br />
+						(negative - expense, positive - income)
+					</label>
+					<input type="number" placeholder="Enter amount..." />
+				</div>
+				<button className="btn">Add transaction</button>
+			</form>
+		</div>
+	);
+};
+
+export default AddTransactionComponent;
