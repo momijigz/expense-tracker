dc3fe2c822aaebc1dc1d6a82e6e63f9b139ff203
Moazzam Khan <moazzam.khan.wa@gmail.com>
2020-02-25 23:43:26 +0500

    Fixing

--- src/components/transaction.component.jsx
+++ src/components/transaction.component.jsx
@@ -7,7 +7,7 @@ const TransactionComponent = ({ transaction }) => {
 	const sign = transaction.amount < 0 ? "-" : "+";
 	return (
 		<li className={transaction.amount < 0 ? "minus" : "plus"}>
-			{transaction.text}{" "}
+			{transaction.text}
 			<span>
 				{sign}${Math.abs(transaction.amount)}
 			</span>
